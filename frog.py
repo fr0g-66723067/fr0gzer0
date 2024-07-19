502070ad55ab36625c93e3d372c51f3f46d5886d
fr0g <fr0g@66723067.io>
2024-07-19 17:40:06 -0600

    add frog files

---
+++ frog.py
@@ -0,0 +1,24 @@
+import logging
+import ST7789
+import time
+
+from PIL import Image, ImageDraw, ImageFont
+
+logging.basicConfig(level=logging.DEBUG)
+# 240x240 display with hardware SPI:
+disp = ST7789.ST7789()
+
+# Initialize library.
+disp.Init()
+
+# Clear display.
+disp.clear()
+
+#Set the backlight to 100
+disp.bl_DutyCycle(50)
+
+logging.info("show image")
+image = Image.open('pic.jpg')
+im_r = image.rotate(270)
+disp.ShowImage(im_r)
+time.sleep(3)
